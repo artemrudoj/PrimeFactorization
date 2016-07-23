9f729fd993bace9f241193c33cdfa825c14b29f9
artem <artem.rudoj@frtk.ru>
2016-07-23 18:43:35 +0300

    add base activities

---
+++ app/src/main/java/com/mipt/artem/primefactorization/base/BackButtonActivity.java
@@ -0,0 +1,29 @@
+package com.mipt.artem.primefactorization.base;
+
+import android.app.FragmentManager;
+import android.view.MenuItem;
+
+
+public class BackButtonActivity extends BaseActivity {
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            fm.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/mipt/artem/primefactorization/base/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.mipt.artem.primefactorization.base;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+
+
+/**
+ *
+ * Created by Vadim Matsishin on 05/12/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
