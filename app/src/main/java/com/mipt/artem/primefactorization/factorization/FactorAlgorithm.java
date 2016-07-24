4ef5b39fe857a3b7730b0f8e245f86eae76d96df
artem <artem.rudoj@frtk.ru>
2016-07-24 16:22:07 +0300

    add progress calculator

--- app/src/main/java/com/mipt/artem/primefactorization/factorization/FactorAlgorithm.java
+++ app/src/main/java/com/mipt/artem/primefactorization/factorization/FactorAlgorithm.java
@@ -1,5 +1,7 @@
 package com.mipt.artem.primefactorization.factorization;
 
+import android.util.Log;
+
 import java.math.BigInteger;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,40 +11,76 @@ import java.util.List;
  * Created by artem on 23.07.16.
  */
 public class FactorAlgorithm {
-    public  static List start(BigInteger n, ProgressChangerListener progressChangerListener) {
-        BigInteger two = BigInteger.valueOf(2);
-        LinkedList fs = new LinkedList();
+    private static final String TAG = "FactorAlgorithm";
 
-        if (n.compareTo(two) < 0)
-        {
+    public  static List start(BigInteger n, ProgressChangeListener progressChangerListener) {
+        BigInteger two = BigInteger.valueOf(2);
+
+        if (n.compareTo(two) < 0) {
             throw new IllegalArgumentException("must be greater than one");
         }
 
-        while (n.mod(two).equals(BigInteger.ZERO))
-        {
+        PercentCalculator percentCalculator = new PercentCalculator(n, progressChangerListener);
+        LinkedList fs = new LinkedList();
+
+
+        while (n.mod(two).equals(BigInteger.ZERO)) {
             fs.add(two);
             n = n.divide(two);
+            percentCalculator.updatePercentAfterNewDiver(n);
         }
 
-        if (n.compareTo(BigInteger.ONE) > 0)
-        {
+        if (n.compareTo(BigInteger.ONE) > 0) {
             BigInteger f = BigInteger.valueOf(3);
-            while (f.multiply(f).compareTo(n) <= 0)
-            {
+            while (f.multiply(f).compareTo(n) <= 0) {
                 if (n.mod(f).equals(BigInteger.ZERO))
                 {
                     fs.add(f);
                     n = n.divide(f);
-                }
-                else
-                {
+                    percentCalculator.updatePercentAfterNewDiver(n);
+                } else {
                     f = f.add(two);
+                    //percentCalculator.updatePercentAfterNewDiver(n);
                 }
 
             }
             fs.add(n);
         }
-
         return fs;
     }
+
+    private static class PercentCalculator {
+        private static final int STEP_PERCENT = 2;
+        private static final int WHOLE_NUMBER_OF_PERCENTS = 100;
+        private ProgressChangeListener mProgressChangerListener;
+        private BigInteger mStartValue;
+        private BigInteger mCurrentValue;
+        private BigInteger mNextStepProgressUpdateValue;
+        private int mCurrentPercentUpdateValue;
+
+        public PercentCalculator(BigInteger n, ProgressChangeListener progressChangerListener) {
+            mStartValue = n;
+            mProgressChangerListener = progressChangerListener;
+        }
+
+        public void updatePercentAfterNewDiver(BigInteger n) {
+            mCurrentValue = n;
+            if(mProgressChangerListener != null) {
+                int progress = 100 - getPartInPercentFromNumber(n, mStartValue);
+                Log.d(TAG, "updatePercentAfterNewDiver = " + Integer.toString(progress));
+                mProgressChangerListener.setProgress(progress);
+            }
+        }
+
+        private int getPartInPercentFromNumber(BigInteger partNumber, BigInteger wholeNumber) {
+            BigInteger percent = partNumber.multiply(BigInteger.valueOf(WHOLE_NUMBER_OF_PERCENTS))
+                    .divide(wholeNumber);
+            int percentCount = percent.intValue();
+            return percentCount;
+        }
+    }
+
+
+
+
 }
